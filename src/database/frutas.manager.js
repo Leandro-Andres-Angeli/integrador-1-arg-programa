a48e33da0ed5e415a4ea90802aa543d1caefb1fe
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-06-26 20:33:26 -0300

    entrega trabajo

--- src/database/frutas.manager.js
+++ src/database/frutas.manager.js
@@ -1,37 +1,112 @@
-const fs = require("fs")
-const path = require("path")
-const route = path.join(__dirname,"frutas.json")
-const read = async()=>{
-        return new Promise((resolve,rej)=>{
-            fs.readFile(route,"utf-8",(err,res)=>{
-                if(err) rej(new Error ("Error leyendo archivo"))
-                resolve(JSON.parse(res))
-            })
-        })
-}
-const write = async(content)=>{
-        return new Promise((resolve,rej)=>{
-            fs.writeFile(route,JSON.stringify(content,null,"\n"),"utf-8",(err,res)=>{
-                if(err) rej(new Error ("Error escribiendo archivo"))
-                resolve(true)
-            })
-        })
-}
-const findAll =async ()=>{
-    const fruits = await read()
+const fs = require("fs");
+const path = require("path");
+const route = path.join(__dirname, "frutas.json");
+const read = async () => {
+  return new Promise((resolve, rej) => {
+    fs.readFile(route, "utf-8", (err, res) => {
+      if (err) rej(new Error("Error leyendo archivo"));
+      resolve(JSON.parse(res));
+    });
+  });
+};
+const write = async (content) => {
+  return new Promise((resolve, rej) => {
+    fs.writeFile(
+      route,
+      JSON.stringify(content, null, "\n"),
+      "utf-8",
+      (err, res) => {
+        if (err) rej(new Error("Error escribiendo archivo"));
+        resolve(true);
+      }
+    );
+  });
+};
+const findAll = async () => {
+  const fruits = await read();
 
-    return fruits
-}
-const addOne = async (fruit)=>{
-    const fruits = await read()
-    console.log(fruits)
-    // fruit.importe&= +fruit.importe
-    // fruit.precio&= +fruit.precio
-    +fruit.importe
-    +fruit.precio
-    const newFruit = {id:fruits.length+1,...fruit}
-    fruits.push(newFruit)
-    await write(fruits)
-    return newFruit 
-}
-module.exports = {findAll, addOne}
+  return fruits;
+};
+const addOne = async (fruit) => {
+  const fruits =await read()
+     fruit.importe&= +fruit.importe
+    
+     fruit.stock&= +fruit.stock
+  
+  const newFruit = { id: fruits.length + 1, ...fruit };
+  fruits.push(newFruit);
+  await write(fruits);
+  return newFruit;
+};
+const updateOne = async (fruitUpdate,id) => {
+  const {nombre,importe,stock} = fruitUpdate
+  if(!nombre || !importe  || !stock){
+    throw new Error("Datos incompletos")
+  }
+  if(!id){
+    throw  new Error("No se encontro fruta con ese Id")
+  }
+  const fruits =await read()
+     fruitUpdate.importe&= +fruitUpdate.importe
+     fruitUpdate.precio&= +fruitUpdate.precio
+     fruitUpdate.stock&= +fruitUpdate.stock
+  
+  if(id < 0 ){
+    throw new Error("Id no existente")
+  }
+  if( fruits.find((fruit) => fruit.id === id) === undefined ){
+    throw new Error("Id no existente") 
+  }
+  const fruitPos = fruits.findIndex((fruit)=> fruit.index === id)
+  fruits[fruitPos] ={id, ...fruitUpdate}
+  await write(fruits);
+  return fruits[fruitPos];
+};
+const findOne = async (id) => {
+  if(id < 0 ){
+    throw new Error("Id no existente")
+  }
+  const fruits = await read();
+  
+  const fruit = fruits.find((fruit) =>{
+   
+    return fruit.id === +id});
+ if(!fruit){
+  throw new Error("No se encontro fruta")
+ }
+  return fruit;
+};
+const findByName = async (name) => {
+  if(!name ){
+    throw new Error("Ingrese nombre")
+  }
+  console.log(name)
+  const fruits = await read();
+  
+  const fruit = fruits.filter((fruit) =>
+
+  fruit.nombre.toLowerCase().includes(name.toLowerCase())
+
+    );
+ if(fruit.length === 0){
+  throw new Error("No se encontraron  frutas")
+ }
+  return fruit;
+};
+const deleteOne = async (id) => {
+  if(!id){
+    throw new Error("No se encontro Id")
+   }
+  const fruits = await read();
+  const idx = fruits.findIndex((el)=> el.id === id)
+  const fruit = fruits[idx]
+  if(!fruit){
+    throw new Error("No se encontro fruta con ese id")
+  }
+  fruits.splice(idx,1)
+  await write(fruits)
+  return fruit
+ 
+
+};
+module.exports = { findAll,findByName, addOne, findOne,updateOne,deleteOne };
--- src/server.js
+++ src/server.js
@@ -1,23 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { findAll, addOne, findOne, updateOne, deleteOne, findByName } = require("./database/frutas.manager");
+require("dotenv").config();
+const server = express();
+server.use(express.json());
+server.use(bodyParser.json());
+server.use(express.urlencoded({ extended: true }));
+server.get("/frutas/", (req, res) => {
+  findAll()
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(400).send(err.message));
+});
+server.get("/frutas/id/:id", (req, res) => {
+  const { id } = req.params;
 
-const express = require("express")
-const bodyParser = require("body-parser")
-const { findAll, addOne } = require("./database/frutas.manager")
-require("dotenv").config()
-const server = express()
-server.use(express.json())
-server.use(bodyParser.json())
-server.use(express.urlencoded({extended:true}))
-server.get("/",(req,res)=>{
-    findAll().then((data)=>res.status(200).send(data)).catch(err => res.status(400).send(err.message))
-})
-server.post("/",(req,res)=>{
-    const {nombre , importe   , stock} = req.body
-    const newFruit = {nombre,importe,stock,imagen:"🍐"}
-    addOne(newFruit).then(data => res.status(201).send(data) ).catch(err => res.status(400).send(err.message))
-    // res.status(200).send(newFruit)
-})
-server.use("*",(req,res)=>{
-    res.status(404).send("Not Found")
-})
+  findOne(id)
+    .then((data) =>{
+     
+        res.status(200).send(data)})
+    .catch((err) => res.status(400).send(err.message));
+});
+server.get("/frutas/name/:name", (req, res) => {
+  const { name } = req.params;
 
-server.listen(process.env.PORT,process.env.HOST , ()=> console.log(`Running on port ${process.env.PORT}`))
+  findByName(name)
+    .then((data) =>{
+      console.log(data)
+        res.status(200).send(data)})
+    .catch((err) => res.status(400).send(err.message));
+});
+server.post("/frutas/", (req, res) => {
+  const { nombre, importe, stock} = req.body;
+  const newFruit = { nombre, importe, stock,imagen: "🍐" };
+  addOne(newFruit)
+    .then((data) => res.status(201).send(data))
+    .catch((err) => res.status(400).send(err.message));
+  
+});
+server.put("/frutas/:id", (req, res) => {
+  const { nombre, importe, stock } = req.body;
+ 
+  const{ id }= req.params
+  const fruitUpdate = { nombre, importe, stock, imagen: "🍐" };
+  updateOne(fruitUpdate,Number(id))
+    .then((data) => res.status(201).send(data))
+    .catch((err) => res.status(400).send(err.message));
+  
+});
+server.delete("/frutas/:id", (req, res) => {
+ 
+  const{ id }= req.params
+ 
+  deleteOne(Number(id))
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(400).send(err.message));
+  
+});
+server.use("*", (req, res) => {
+  res.status(404).send("Not Found");
+});
+
+
+server.listen(process.env.PORT, process.env.HOST, () =>
+  console.log(`Running on port ${process.env.PORT}`)
+);
